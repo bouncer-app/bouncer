01d575fd53f806631af420a6f3c683f2ae033002
Jonathan Tushman <jtushman@pipewave.com>
2014-03-20 13:55:28 -0400

    removing unused callback

--- flask_bouncer/flask_bouncer.py
+++ flask_bouncer/flask_bouncer.py
@@ -38,8 +38,6 @@ class Bouncer(object):
 
         self.endpoint_dict = dict()
 
-        self.authorization_target_callback = None
-
         self.authorization_method_callback = None
 
 
@@ -57,16 +55,8 @@ class Bouncer(object):
         return decorator
 
     def init_app(self, app):
-        # I am sure that we will need to put something in here soon
         app.before_request(self.check_abilities)
 
-    def authorization_target(self, callback):
-        """
-        the callback for finding the currentuser in the given request context.
-        should return `None` if the user does not exist
-        """
-        self.authorization_target_callback = callback
-        return callback
 
     def authorization_method(self, callback):
         """
