9087ade118e7dcd30cee23b4ca8c1d9d10362be9
Jonathan Tushman <jtushman@pipewave.com>
2014-04-02 09:15:07 -0400

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 required_modules = []
 
 setup(name='bouncer',
-      version='0.1.8',
+      version='0.1.9',
       description='Simple Declarative Authentication based on Ryan Bates excellent cancan library',
       url='http://github.com/jtushman/bouncer',
       author='Jonathan Tushman',
